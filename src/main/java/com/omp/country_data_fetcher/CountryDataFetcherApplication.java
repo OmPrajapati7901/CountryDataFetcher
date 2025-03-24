f86e1205fbf4b129b4f1eba51cb40c51261da529
OmPrajapati7901 <omprajapati7901@gmail.com>
2025-03-24 11:24:07 -0400

    Initial commit

---
+++ src/main/java/com/omp/country_data_fetcher/CountryDataFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.omp.country_data_fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDataFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryDataFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omp/country_data_fetcher/CountryDataFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omp.country_data_fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryDataFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
